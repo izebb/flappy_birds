7abd1786aaf7ae3a6353daacec40eaf1f41615eb
izebb <izebhigie@gmail.com>
2021-11-01 20:34:19 +0100

    fix: Added more print functions

--- src/main.rs
+++ src/main.rs
@@ -19,33 +19,35 @@ struct Obstacle {
 impl Obstacle {
     fn new(x: i32, score: i32) -> Self {
         let mut random = RandomNumberGenerator::new();
-        Obstacle {
+
+        Self {
             x,
             gap_y: random.range(10, 40),
             size: i32::max(2, 20 - score),
         }
     }
 
-    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
+    fn render(&mut self, player_x: i32, ctx: &mut BTerm) {
         let screen_x = self.x - player_x;
         let half_size = self.size / 2;
 
         for y in 0..self.gap_y - half_size {
-            ctx.set(screen_x, y, RED, BLACK, to_cp437('/'));
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
         }
 
-        for y in self.gap_y + half_size..SCREEN_HEIGHT {
-            ctx.set(screen_x, y, RED, BLACK, to_cp437('/'));
+        for y in (self.gap_y + half_size)..SCREEN_HEIGHT {
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'))
         }
     }
 
     fn hit_obstacle(&self, player: &Player) -> bool {
         let half_size = self.size / 2;
-        let does_x_match = player.x == self.x;
+        let is_x_equal = player.x == self.x;
+
         let player_above_gap = player.y < self.gap_y - half_size;
         let player_below_gap = player.y > self.gap_y + half_size;
 
-        does_x_match && (player_above_gap || player_below_gap)
+        is_x_equal && (player_above_gap || player_below_gap)
     }
 }
 
@@ -57,7 +59,7 @@ struct Player {
 
 impl Player {
     fn new(x: i32, y: i32) -> Self {
-        Player {
+        Self {
             x,
             y,
             velocity: 0.0,
@@ -65,7 +67,7 @@ impl Player {
     }
 
     fn render(&mut self, ctx: &mut BTerm) {
-        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'))
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'));
     }
 
     fn gravity_and_move(&mut self) {
@@ -87,11 +89,33 @@ impl Player {
 }
 
 struct State {
-    player: Player,
     frame_time: f32,
     mode: GameMode,
-    score: i32,
     obstacle: Obstacle,
+    player: Player,
+    score: i32,
+}
+
+fn print_play_or_quit(ctx: &mut BTerm) {
+    ctx.print_centered(8, "(P) Play game");
+    ctx.print_centered(9, "(Q) Quit game");
+}
+
+fn print_welcome(ctx: &mut BTerm) {
+    ctx.print_centered(5, "Welcome to Flappy bird");
+}
+
+fn print_control_key(ctx: &mut BTerm) {
+    ctx.print(0, 0, "Press SPACE key to flap your wings");
+}
+
+fn print_score(ctx: &mut BTerm, score: i32) {
+    ctx.print(0, 1, &format!("Your Score {} points", score))
+}
+
+fn print_game_over(ctx: &mut BTerm, score: i32) {
+    ctx.print_centered(5, "Game over! you lost :(");
+    ctx.print(0, 1, &format!("Your earn {} points", score));
 }
 
 impl State {
@@ -105,19 +129,12 @@ impl State {
         }
     }
 
-    fn main_menu(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print_centered(5, "Welcome to flappy Dragon");
-        ctx.print_centered(8, "(P) Play Game");
-        ctx.print_centered(9, "(Q) Quit Game");
-
-        if let Some(key) = ctx.key {
-            match key {
-                VirtualKeyCode::P => self.restart(),
-                VirtualKeyCode::Q => ctx.quitting = true,
-                _ => {}
-            }
-        }
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
+        self.frame_time = 0.0;
+        self.score = 0;
+        self.player = Player::new(5, 25);
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
@@ -126,7 +143,6 @@ impl State {
 
         if self.frame_time > FRAME_DURATION {
             self.frame_time = 0.0;
-
             self.player.gravity_and_move();
         }
 
@@ -136,13 +152,14 @@ impl State {
 
         self.player.render(ctx);
 
-        ctx.print(0, 0, "Press SPACE to flap");
-        ctx.print(0, 1, format!("score {}", self.score));
+        print_control_key(ctx);
+        print_score(ctx, self.score);
 
-        self.obstacle.render(ctx, self.player.x);
+        self.obstacle.render(self.player.x, ctx);
+
         if self.player.x > self.obstacle.x {
             self.score += 1;
-            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score)
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
         }
 
         if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
@@ -150,12 +167,10 @@ impl State {
         }
     }
 
-    fn dead(&mut self, ctx: &mut BTerm) {
+    fn main_menu(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print_centered(5, "You are dead!");
-        ctx.print_centered(6, format!("You earned {} points", self.score));
-        ctx.print_centered(8, "(P) Play again");
-        ctx.print_centered(9, "(Q) Quit Game");
+        print_welcome(ctx);
+        print_play_or_quit(ctx);
 
         if let Some(key) = ctx.key {
             match key {
@@ -166,12 +181,18 @@ impl State {
         }
     }
 
-    fn restart(&mut self) {
-        self.player = Player::new(5, 25);
-        self.frame_time = 0.0;
-        self.mode = GameMode::Playing;
-        self.score = 0;
-        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        print_game_over(ctx, self.score);
+        print_play_or_quit(ctx);
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
     }
 }
 
@@ -179,16 +200,15 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         match self.mode {
             GameMode::Menu => self.main_menu(ctx),
-            GameMode::End => self.dead(ctx),
             GameMode::Playing => self.play(ctx),
+            GameMode::End => self.dead(ctx),
         }
     }
 }
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
-        .with_title("Flappy Dragon")
+        .with_title("Flappy birds")
         .build()?;
-
     main_loop(context, State::new())
 }
